b0e5b13640d80fafd194ef4dde9e2f80a63ecccc
wjonke <wesjonke@gmail.com>
2020-10-22 10:35:06 -0400

    basic import practice using es6 syntax

--- gqlBasics/src/modules.js
+++ gqlBasics/src/modules.js
@@ -1,3 +1,8 @@
-const hello = '--------------- >  Hello World <---------------';
+const hello = 'Hello';
+const name = 'Wes';
+const location = 'Atlanta';
+const getGreeting = (hello, name, location) => {
+  return `${hello} ${name}, I see you are in ${location}, welcome to the course`;
+};
 
-export { hello };
+export { hello, name, location, getGreeting };
--- gqlBasics/src/server.js
+++ gqlBasics/src/server.js
@@ -1,3 +1,7 @@
-import { hello } from './modules';
+import { hello, name, location, getGreeting } from './modules';
 
 console.log(hello);
+console.log(name);
+console.log(location);
+
+console.log(getGreeting('Hi', 'Jim', 'Toledo'));
